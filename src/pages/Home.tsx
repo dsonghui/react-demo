32326f4b8c10a2fedafefe0058dc55ea66ced9a1
David <david.deng@jcinfotech.com>
2018-12-16 22:19:17 +0800

    测试 mobx-react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,27 @@ import { inject, observer } from "mobx-react";
 import { StoreType } from "@/store";
 
 
-interface MyComponentProps {
+interface IMyComponentProps {
     myProps: { str: string };
 }
 
 @inject('Store')
 @observer
-export default class Home<T extends { Store: StoreType }> extends React.Component<T & MyComponentProps, {}> {
+export default class Home<T extends { Store: StoreType }> extends React.Component<T & IMyComponentProps, {}> {
     constructor(props: any) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick() {
+        this.props.Store.updateLoading(!this.props.Store.loading);
+    }
+
     public render() {
         console.log(this.props.Store.loading);
         return <div>
-            Home
+            {this.props.Store.loading}
+            <div onClick={this.handleClick}>Click</div>
         </div>
     }
 }
